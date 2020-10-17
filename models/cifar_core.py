635d498bdf55bcb0a0477eb10bd0ab68b6ace59e
Rishabh Sharma <rishabh.2000.rs@gmail.com>
2020-10-17 13:00:03 +0530

    Update cifar_core.py

--- models/cifar_core.py
+++ models/cifar_core.py
@@ -256,18 +256,8 @@ class CifarModel():
         # Test and save the result
         test_color_result = self._test(self.test_color_loader)
         test_gray_result = self._test(self.test_gray_loader)
-        
-#         import pickle
-#         import numpy as np
-#         test_color='/content/drive/My Drive/drip_copy/fair_lamda_5_exp3/record/cifar-s_attention/attention_1/test_color_result.pkl'
-#         test_gray='/content/drive/My Drive/drip_copy/fair_lamda_5_exp3/record/cifar-s_attention/attention_1/test_gray_result.pkl'
-        
-#         bias=0
-#         with open(test_color, 'rb') as f:
-#               test_color_result = pickle.load(f)
-#         with open(test_gray, 'rb') as f:
-#               test_gray_result = pickle.load(f)
-
+        import numpy as np
+        bias=0
         for i in range(10):
           Gr=(np.array(test_gray_result['predict_labels'])==i).sum()
           Col=(np.array(test_color_result['predict_labels'])==i).sum()
