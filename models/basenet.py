a6dd58be8b66c8ffcd0e6049618131d01aeea6f6
Rishabh Sharma <rishabh.2000.rs@gmail.com>
2020-10-18 23:27:38 +0530

    Update basenet.py

--- models/basenet.py
+++ models/basenet.py
@@ -68,7 +68,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-#         self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear(512*block.expansion, 1000)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -86,8 +86,8 @@ class ResNet(nn.Module):
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         feature = out.view(out.size(0), -1)
-#         out = self.linear(feature)
-        return feature
+        out = self.linear(feature)
+        return out
 
 class ResNet_base(nn.Module):
     def __init__(self, block, num_blocks):
